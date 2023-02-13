36e80eb893eca15dd34ccf6e08ad437c1b88f9f3
Cristian Rodriguez <cristiandosespigas@gmail.com>
2023-02-13 15:57:08 -0300

    first commit

---
+++ src/main/java/com/example/HayEquipo/HayEquipoApplication.java
@@ -0,0 +1,13 @@
+package com.example.HayEquipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HayEquipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HayEquipoApplication.class, args);
+	}
+
+}
